5fa023380cd0ec6e90a9614655fb54abba2f5f75
itsmesauravk <np03cs4s230139@heraldcollege.edu.np>
2023-04-02 22:25:47 +0545

    class pythons

---
+++ for count vowel loop.py
@@ -0,0 +1,11 @@
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def vowel()
+SyntaxError: incomplete input
+>>> def vowel():
+...     vo=0
+...     tex=input('any:')
+...     while tex !=0:
+...         tex=input('any:')
+...         if tex == 'a':
+...             vo = vo+1
---
+++ print largest digit in loop.py
@@ -0,0 +1,9 @@
+product=2
+num=int(input('enter:'))
+while num !=0:
+    num=int(input('enter:'))
+    product *= num
+    print(product)
+       
+    
+        
