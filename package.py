7bec1bf27bd4e4d3c7d73afb9342ab75cf436a8f
Saurav Karki <127291490+itsmesauravk@users.noreply.github.com>
2023-06-17 21:13:10 +0545

    Add files via upload

---
+++ package.py
@@ -0,0 +1,6 @@
+from modules_import.import_module import name
+#in above i am importing form package i have created by my self
+#first is package name and then module name after that function name
+# from modules_import import import_modules  >> this is another way
+#while importing from above type we should have to write in >> import_module.name()
+name()
