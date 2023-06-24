fe4ae3199b22ef1c5711f4aebdad2ac26d7877fe
Saurav Karki <127291490+itsmesauravk@users.noreply.github.com>
2023-06-24 20:16:49 +0545

    Add files via upload

---
+++ import_module.py
@@ -0,0 +1,11 @@
+def max_number(numbers):
+    maximum = numbers[0]
+    for num in numbers:
+        if num>maximum:
+            maximum = num
+        else:
+            maximum = maximum
+    return maximum
+
+def name():
+    print("Hola mi aamo saurav. mucho gusto. como estas. hablas inglish? buenas noches, adios !!")
---
+++ result_module.py
@@ -0,0 +1,6 @@
+# import import_module
+from import_module import max_number
+
+numbers = [1,3,5,2]
+maximum = max_number(numbers)
+print(maximum) 
