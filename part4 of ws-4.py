7d843aab04c8f214cb89ee92cecd36dbc81afba6
Saurav Karki <127291490+itsmesauravk@users.noreply.github.com>
2023-06-25 21:25:01 +0545

    Add files via upload

---
+++ part4 of ws-4.py
@@ -0,0 +1,19 @@
+for row in range(0,11):
+    for col in range(0,11):
+        num=row*col
+        if num<10:
+            empty=' '
+        else:
+            if num<100:
+                empty=' '
+        if col==0:
+            if row ==0:
+                print(' ',end=" ")
+            else:
+                print(' ',row, end=" ")
+        elif row==0:
+            print(' ',col,end=" ")
+        else:
+            print(empty,num,end=" ")
+    print()
+    
